1e97a7d62d4a6a162fdc38adace415802ec5bdeb
majdiJ <70067205+majdiJ@users.noreply.github.com>
2025-04-25 16:27:28 +0100

    Welcome screen text

--- smart fitness tracker application/src/WelcomeScreen.java
+++ smart fitness tracker application/src/WelcomeScreen.java
@@ -12,12 +12,16 @@ public class WelcomeScreen {
     private int width;
     private int height;
     private JLabel welcomeTitleTextLabel;
+    private JLabel welcomeParagraphTextLabel;
 
     public WelcomeScreen(int w, int h) {
         frame = new JFrame();
         width = w;
         height = h;
         welcomeTitleTextLabel = new JLabel("Welcome to FitSmart", JLabel.CENTER);
+        welcomeTitleTextLabel.setBounds(0, 50, width, 50);
+        welcomeParagraphTextLabel = new JLabel("Your smart fitness tracking companion", JLabel.CENTER);
+        welcomeParagraphTextLabel.setBounds(0, 100, width, 50);
     }
 
     public void display() {
@@ -27,6 +31,7 @@ public class WelcomeScreen {
         frame.setResizable(false);
 
         frame.add(welcomeTitleTextLabel);
+        frame.add(welcomeParagraphTextLabel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true); // Show the frame
